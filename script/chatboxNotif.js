6e777023e41245243b68952cc2231d72244dc865
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-02-13 19:41:24 +0100

    Add files via upload

---
+++ script/chatboxNotif.js
@@ -0,0 +1,39 @@
+const chatboxElt = document.getElementById("chatbox");
+const notificationSound = new Audio("https://www.myinstants.com/media/sounds/cannard.mp3");
+const inputElt = document.getElementById("message");
+const windowURL = "https://ashes-arise.forumactif.com/chatbox/";
+const easterEgg = new Audio("https://www.myinstants.com/media/sounds/puissance-et-gloire.mp3");
+
+//Vérifie si la fenêtre chatbox est cachée
+document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState === "hidden") {
+        //Créé un objet observant la chatbox
+        let observer = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                //Vérifie si des enfants sont générés
+                if (mutation.addedNodes
+                    && document.visibilityState === "hidden"
+                    && window.location.href == windowURL) {
+                    document.title = "NOUVEAUX MESSAGES";
+                    notificationSound.play();
+                };
+            });
+        });
+        //Paramètres à observer
+        observer.observe(chatboxElt, {
+            childList: true
+        });
+
+    } else {
+        //Remet à zéro la chatbox si affichée
+        document.title = "ChatBox";
+        delete MutationObserver;
+    };
+});
+
+//Easter Egg
+inputElt.addEventListener("change", () => {
+    if (inputElt.value == "/PUISSANCE") {
+        easterEgg.play();
+    };
+})
