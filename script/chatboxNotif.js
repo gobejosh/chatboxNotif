f636ecf4c487f1390b3c676be926a98b8ae4abec
nicolasgen4 <121958459+nicolasgen4@users.noreply.github.com>
2023-02-14 18:31:33 +0100

    Update chatboxNotif.js

--- script/chatboxNotif.js
+++ script/chatboxNotif.js
@@ -2,11 +2,15 @@ const chatboxElt = document.getElementById("chatbox");
 const inputElt = document.getElementById("message");
 //Fichiers MP3
 const notificationSound = new Audio("https://assets.mixkit.co/active_storage/sfx/2358/2358-preview.mp3");
-const easterEgg = new Audio("https://www.myinstants.com/media/sounds/puissance-et-gloire.mp3");
+const puissanceEE = new Audio("https://www.myinstants.com/media/sounds/puissance-et-gloire.mp3");
+const wizzEE = new Audio("https://www.myinstants.com/media/sounds/msn-wizz-sound.mp3");
+const hahaEE = new Audio("https://www.myinstants.com/media/sounds/the-simpsons-nelsons-haha.mp3");
+const coinEE = new Audio("https://www.myinstants.com/media/sounds/couin.mp3");
+const chutEE = new Audio("https://www.myinstants.com/media/sounds/ta-gueule_6iavH8Q.mp3");
 //Fenêtres autorisées
 const windowsURL = [
-    "URL1",
-    "URL2"
+    "URL",
+    "URL"
 ];
 
 //Vérifie le statut de la fenêtre chatbox
@@ -39,6 +43,14 @@ document.addEventListener("visibilitychange", () => {
 //Easter Egg
 inputElt.addEventListener("change", () => {
     if (inputElt.value == "/PUISSANCE") {
-        easterEgg.play();
-    };
-})
+        puissanceEE.play();
+    } else if (inputElt.value == "/WIZ") {
+        wizzEE.play();
+    } else if (inputElt.value == "/HAHA") {
+        hahaEE.play();
+    } else if (inputElt.value == "/COIN") {
+        coinEE.play();
+    } else if (inputElt.value == "/CHUT") {
+        chutEE.play();
+    }
+});
